797327bdc101435e9c142625edce7286f61b12dc
chromia <chromia@outlook.jp>
2020-07-26 11:48:27 +0900

    add scale argument to FastDBM make_bias

--- lichtenberg/fastdbm.py
+++ lichtenberg/fastdbm.py
@@ -8,18 +8,18 @@ class BiasDirection(Enum):
     Down = 4
 
 
-def make_bias(direction: BiasDirection, width: int, height: int):
+def make_bias(direction: BiasDirection, width: int, height: int, scale: float = 1.0):
     bias = []
     for y in range(height):
         for x in range(width):
             if direction == BiasDirection.Left:
-                b = 1.0 + 1.0 - x/width
+                b = 1.0 + (1.0 - x/width) * scale
             elif direction == BiasDirection.Right:
-                b = 1.0 + x/width
+                b = 1.0 + (x/width) * scale
             elif direction == BiasDirection.Up:
-                b = 1.0 + 1.0 - y/height
+                b = 1.0 + (1.0 - y/height) * scale
             elif direction == BiasDirection.Down:
-                b = 1.0 + y/height
+                b = 1.0 + (y/height) * scale
             else:
                 b = 1.0
             bias.append(b)
